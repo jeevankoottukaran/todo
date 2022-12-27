2048f1592636999bcfecf57812697f476a6e9433
jeevankoottukaran <121094076+jeevankoottukaran@users.noreply.github.com>
2022-12-27 12:03:42 +0530

    Delete dmproject directory

--- dmproject/demoapp/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Task(models.Model):
-    name=models.CharField(max_length=250)
-    priority=models.IntegerField(null=True)
-    date=models.DateField()
